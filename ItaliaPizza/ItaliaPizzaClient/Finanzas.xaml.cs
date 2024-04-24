9b722db675a6f041920c85124a31f661f35e10f7
alvaroLopezM <131178547+alvaroLopezM@users.noreply.github.com>
2024-04-23 21:39:29 -0600

    Commit CU Proveedor | Pedidos

---
+++ ItaliaPizza/ItaliaPizzaClient/Finanzas.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ItaliaPizzaClient
+{
+    /// <summary>
+    /// Interaction logic for Finanzas.xaml
+    /// </summary>
+    public partial class Finanzas : UserControl
+    {
+        public Finanzas()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnProveedores_Click(object sender, RoutedEventArgs e)
+        {
+            contentControl.Content = new ListaProveedores();
+        }
+    }
+}
---
+++ ItaliaPizza/Services/IOrderManager.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    [ServiceContract]
+    public interface IOrderManager
+    {
+        [OperationContract]
+        void RegistrarNuevoPedido(string nombreCliente, int idEmpleado, string domicilio, string tipoPedido, decimal total, List<int> idProductos);
+
+        [OperationContract]
+        List<Logic.Pedidos> RecuperarInformacionPedidos();
+
+        [OperationContract]
+        bool ModificarEstadoDePedido(int pedidoId, string nuevoEstado);
+
+        [OperationContract]
+        List<Logic.Productos> RecuperarInformacionProductos(string tipo);
+        [OperationContract]
+        void ActualizarPedido(int idPedido, string nombreCliente, string domicilio, string tipoPedido, decimal total, List<int> idProductos);
+        [OperationContract]
+        List<int> RecuperarIdsProductosDePedido(int idPedido);
+        [OperationContract]
+        Logic.Productos RecuperarProductoPorId(int idProducto);
+    }
+}
